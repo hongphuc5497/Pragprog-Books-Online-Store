83c3d645bba4a2093af542ef5e6ec671d62504b8
hongphuc <hongphuc.dthp@gmail.com>
2019-07-25 11:55:04 +0700

    Make shopping cart smarter

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -16,6 +16,7 @@ class StoreControllerTest < ActionController::TestCase
     assert_select '#main .entry', 3
     assert_select 'h3', 'Programming Ruby 1.9'
     assert_select '.price', /\$[,\d]+\.\d\d/
+    
   end
 
 end
